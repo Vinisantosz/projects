61a7272969579fc26e1c2beffefbde5fb7320e16
Vini Santos <124101031+Vinisantosz@users.noreply.github.com>
2023-03-11 00:01:31 -0300

    Update main.py

--- loja/admin/main.py
+++ loja/admin/main.py
@@ -1,10 +1,15 @@
 import login
+from tkinter import *
+
 
 def main():
+
+    janela = Tk()
+    janela.title("LOGIN")
+    janela.mainloop()
 
     # GARANTE QUE O USUARIO NÃO TENTE EXECULTAR FUNÇÕES SEM QUE ESTAR LOGADO 
     while(login.login() == False):
         print("usuario ou senha invalido")
-    
 
 main()
